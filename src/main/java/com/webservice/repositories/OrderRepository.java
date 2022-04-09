cb0725bccfe3b96adbd0e89e6773cbec99da72dc
brenobss <brenobarreto5@hotmail.com>
2022-04-09 19:25:33 -0300

    add Order service resource and repository and add order tests

---
+++ src/main/java/com/webservice/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservice.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+}
